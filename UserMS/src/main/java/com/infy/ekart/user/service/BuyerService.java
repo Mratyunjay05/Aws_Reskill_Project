1d33ded40efed20e695fe920e9ca9c8e4c5f1fbe
rahul <rahulpatidar1998@gmail.com>
2021-05-15 03:10:37 +0530

    added new method in BuyerService

--- UserMS/src/main/java/com/infy/ekart/user/service/BuyerService.java
+++ UserMS/src/main/java/com/infy/ekart/user/service/BuyerService.java
@@ -1,9 +1,10 @@
 package com.infy.ekart.user.service;
 
 import com.infy.ekart.user.dto.BuyerDTO;
+import com.infy.ekart.user.exception.EkartException;
 
 public interface BuyerService {
-	public BuyerDTO loginBuyer(String email, String password);
+	public BuyerDTO loginBuyer(String email, String password) throws EkartException;
 	
-	public String registerBuyer(BuyerDTO buyer); 
+	public String registerBuyer(BuyerDTO buyer) throws EkartException; 
 }
