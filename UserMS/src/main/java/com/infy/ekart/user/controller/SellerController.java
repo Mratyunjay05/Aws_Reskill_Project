4a7439ad352baafd269c6bb5a1f7a94c72fdac50
mratyunjay <kushjadoun@gmail.com>
2021-05-15 17:12:07 +0530

    created controller for seller

--- UserMS/src/main/java/com/infy/ekart/user/controller/SellerController.java
+++ UserMS/src/main/java/com/infy/ekart/user/controller/SellerController.java
@@ -1,5 +1,31 @@
 package com.infy.ekart.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.infy.ekart.user.dto.SellerDTO;
+import com.infy.ekart.user.exception.EkartException;
+import com.infy.ekart.user.service.SellerService;
+
+@RestController
+@RequestMapping(value = "/Seller")
 public class SellerController {
 
+	@Autowired
+	private SellerService sellerService;
+	
+
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public ResponseEntity<SellerDTO> buyerLogin(@RequestBody SellerDTO seller) throws EkartException{
+			
+		SellerDTO sellerServ =  sellerService.loginSeller(seller.getEmail(), seller.getPassword());
+		return new ResponseEntity<>(sellerServ, HttpStatus.OK);
+		
+	}
+	
 }
